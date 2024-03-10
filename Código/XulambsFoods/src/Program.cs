246cba3827bd6d4d0d30bfbf58e859b63abaa499
João Caram <caram@pucminas.br>
2024-03-10 12:49:27 -0300

    Classe pedido e testes

--- Código/XulambsFoods/src/Program.cs
+++ Código/XulambsFoods/src/Program.cs
@@ -2,15 +2,14 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
+        static void testeComida() {
             Comida comida = new Comida("Pizza", 3);
             Console.WriteLine(comida.relatorio());      // 3 ingredientes, R$41
-            
+
             comida.adicionarIngredientes(4);            // 7 ingredientes, R$57 
             Console.WriteLine(comida.relatorio());
-            
-            comida.adicionarIngredientes(4);        
+
+            comida.adicionarIngredientes(4);
             Console.WriteLine(comida.relatorio());      // 7 ingredientes, R$57, não pode ultrapassar o limite.
 
             comida = new Comida("sanDUICHE");
@@ -24,6 +23,35 @@
 
             comida = new Comida();
             Console.WriteLine(comida.relatorio());      //0 ingredientes, R$29
+        }
+
+        static void testePedido() {
+            Pedido ped = new Pedido();
+            Comida pizza = new Comida("PizzA", 3);
+
+            ped.addComida(pizza);
+
+            for(int i=0; i<12; i++) {
+                pizza = new Comida("PIZZa", i / 2);
+                ped.addComida(pizza);
+            }
+
+            Console.WriteLine(ped.relatorio());     // Pedido nº1 com 10 pizzas (limite de comidas)
+
+            ped = new Pedido();
+            ped.addComida(pizza);
+            ped.fecharPedido();
+            ped.addComida(pizza);
+
+            Console.WriteLine(ped.relatorio());     // Pedido nº2 com 1 pizza, pois estava fechado
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            testePedido();
+            Console.ReadKey();
 
         }
     }
