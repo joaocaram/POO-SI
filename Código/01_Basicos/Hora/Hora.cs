5a8aef4f1495054eb127a574d8c0f15547159e4c
João Caram <caram@pucminas.br>
2024-08-25 21:55:40 -0300

    Atualização do exemplo Hora

--- Código/01_Basicos/Hora/Hora.cs
+++ Código/01_Basicos/Hora/Hora.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 /** 
  * MIT License
@@ -40,144 +41,94 @@ namespace Hora {
         /// <summary>
         /// Representa o componente Hora (entre 0 e 23)
         /// </summary>
-        private byte _hora;
-
-        public byte hora{
-            get => _hora;
-            set{
-                if (value >= 0 && value <= 23)
-                    _hora = value;
-                else
-                    _hora = 0;
-            }
-
-        }
-
+        public int horas;
         /// <summary>
         /// Representa o componente Minuto (entre 0 e 59)
         /// </summary>
-        private byte _minuto;
+        public int minutos;
         /// <summary>
         /// Representa o componente Segundo (entre 0 e 59)
         /// </summary>
-        private byte _segundo;
+        public int segundos;
         #endregion
 
         #region Métodos de negócio
         /// <summary>
-        /// Faz a validação da hora atribuída. Em caso de hora, _minuto ou _segundo inválidos, sem reverter o horário em caso de não-validação.
-        /// </summary>
-        /// <returns>TRUE ou FALSE conforme a hora atual é válida ou não </returns>
-        public bool validar() {
-            bool resposta = false;
-            if ((_hora >= 0 && _hora <= 23) && (_minuto >= 0 && _minuto <= 59)
-                     && (_segundo >= 0 && _segundo <= 59)) {
-                resposta = true;
-            }
-            return resposta;       
-        }
-
-        /// <summary>
         /// Ajusta o horário com os parâmetros passados pelo usuário. Chama o método de validação e, em caso de não-validação, reverte o horário para 00:00:00 (meia noite)
         /// </summary>
-        /// <param name="hora">A hora para ser ajustada (0 a 23)</param>
-        /// <param name="minuto">O _minuto para ser ajustado (0 a 59)</param>
-        /// <param name="segundo">O _segundo para ser ajustado (0 a 59)</param>
-        public void ajustar(byte hora, byte minuto, byte segundo) {
-            this._hora = hora;
-            this._minuto = minuto;
-            this._segundo = segundo;
-            if (!validar()) {
-                this._hora = this._minuto = this._segundo = 0;
+        /// <param name="hora">A horas para ser ajustada (0 a 23)</param>
+        /// <param name="min">O minutos para ser ajustado (0 a 59)</param>
+        /// <param name="seg">O segundos para ser ajustado (0 a 59)</param>
+        public void Ajustar(int hora, int min, int seg) {
+            this.horas = hora;
+            this.minutos = min;
+            this.segundos = seg;
+            if (!Validar()) {
+                this.horas = this.minutos = this.segundos = 0;
             }
         }
-       
-        /// <summary>
-        /// Retorna uma string com a hora formatada como HH:MM:SS.
-        /// </summary>
-        /// <returns>String no formato HH:MM:SS</returns>
-        public string horaFormatada() {
-            String horaF = _hora.ToString("00");
-            String minF = _minuto.ToString("00");
-            String segF = _segundo.ToString("00");
 
-            return horaF + ":" + minF + ":" + segF;
 
-        }
-       
         /// <summary>
-        /// Verifica se esta hora está na frente (em horário de relógio) de outra hora recebida
-        /// por parâmetro. Retorna verdadeiro se a hora estiver estritamente na frente. 
+        /// Faz a validação da horas atribuída. Em caso de horas, minutos ou segundos inválidos, sem reverter o horário em caso de não-validação.
         /// </summary>
-        /// <param name="outra">A outra hora para ser comparada com esta.</param>
-        /// <returns>TRUE se esta hora está mais à frente, FALSE caso contrário.</returns>
-        public bool estahNaFrente(Hora outra) {
+        /// <returns>TRUE ou FALSE conforme a horas atual é válida ou não </returns>
+        public bool Validar() {
+            //TODO: revisar o código para melhorá-lo
             bool resposta = false;
-            
-            int estaHora = _segundo + _minuto * 60 + _hora * 3600;
-            int outraHora = outra._segundo + outra._minuto * 60 + outra._hora * 3600;
-
-            if (estaHora > outraHora)
-                resposta = true;
-
-            //String estaHoraString = hora.ToString("00") + _minuto.ToString("00") + _segundo.ToString("00");
-            //String outraHoraString = outra.hora.ToString("00") + outra._minuto.ToString("00") + outra._segundo.ToString("00");
-
-            //if(estaHoraString.CompareTo(outraHoraString)>0)
-            //    resposta = true;
-
-            //if (hora > outra.hora)
-            //    resposta = true;
-            //else if (hora == outra.hora && _minuto > outra._minuto)
-            //    resposta = true;
-            //else if (hora == outra.hora && _minuto == outra._minuto && _segundo > outra._segundo)
-            //    resposta = true;
-
+            if ((horas >= 0 && horas <= 23) && 
+                (minutos >= 0 && minutos <= 59) && 
+                (segundos >= 0 && segundos <= 59)) {
+                    resposta = true;
+            }
             return resposta;
         }
-        
+
         /// <summary>
-        /// Incrementa uma quantidade inteira em algum dos componentes da hora (hora, _minuto
-        /// ou _segundo). Ajusta automaticamente o horário resultante para respeitar as regras de 24h, 
-        /// 60min e 60seg. 
+        /// Incrementa uma quantidade de segundos a uma hora existente, retornando a nova hora. Não modifica a hora já existente.
+        /// Quantidades menores ou iguais a 0 são ignoradas e é retornada uma hora idêntica à atual.
         /// </summary>
-        /// <param name="quant"></param>
-        /// <param name="posicao"></param>
-        public void incrementar(byte quant, char posicao) {
-            string pos = posicao.ToString().ToLower();
-            
-            switch (pos) {
-                case "h": _hora += quant;    
-                    break;
-                case "m": _minuto += quant;
-                    break;
-                case "s": _segundo += quant;
-                    break;
+        /// <param name="quantidadeSegundos">Quantidade de segundos a incrementar (>0)</param>
+        /// <returns>Uma nova hora com os segundos incrementados, podendo ser igual à hora atual em caso de valores não positivos.</returns>
+        public Hora Incrementar(int quantidadeSegundos) {
+            //TODO: revisar o código para melhorá-lo
+            int minutosParaSegundos = 60;
+            int horasParaMinutos = 60;
+            int horasParaSegundos = minutosParaSegundos * horasParaMinutos;
+            Hora novaHora = new Hora();
+            novaHora.Ajustar(horas, minutos, segundos);
+
+            if (quantidadeSegundos > 0) {
+                int totalSegundos = (horas * horasParaSegundos + minutos * minutosParaSegundos + segundos) + quantidadeSegundos;
+                int novosSegundos = totalSegundos % 60;
+
+                int novosMinutos = (totalSegundos / minutosParaSegundos) % horasParaMinutos;
+                int novasHoras = (totalSegundos / horasParaSegundos) % 24;
+
+                novaHora.Ajustar(novasHoras, novosMinutos, novosSegundos);
             }
-            ajustarIncremento();
+            return novaHora;
+        }
+
+        public bool EstahNaFrenteDe(Hora outra) {
+            //TODO: revisar o código para melhorá-lo
+            int minutosParaSegundos = 60;
+            int horasParaSegundos = minutosParaSegundos * 60;
+
+            int esta;
+            esta = (horas * horasParaSegundos + minutos * minutosParaSegundos + segundos);
+            int aquela;
+            aquela = outra.horas * horasParaSegundos + outra.minutos * minutosParaSegundos + outra.segundos;
+            return (esta > aquela);
         }
 
         /// <summary>
-        /// Método privado que ajusta a hora após o incremento, verificando os limites de segundos e _minuto (59) e de hora (23).
-        /// Ajusta os atributos diretamente usando operações de divisão e resto, de modo que não é necessário retorno de valores.
+        /// Retorna uma string com a horas formatada como HH:MM:SS.
         /// </summary>
-        private void ajustarIncremento() {
-            int vaiUm, resto;
-            
-            vaiUm = _segundo / 60;
-            resto = _segundo % 60;
-            _segundo = (byte)resto;
-            _minuto += (byte)vaiUm;
-
-            vaiUm = _minuto / 60;
-            resto = _minuto % 60;
-            _minuto = (byte)resto;
-            _hora += (byte)vaiUm;
-
-            resto = hora % 24;
-            _hora = (byte)resto;
+        /// <returns>String no formato HH:MM:SS</returns>
+        public string HoraFormatada() {
+              return $"{horas:D2}:{minutos:D2}:{segundos:D2}";
         }
-
         #endregion
     }
 }
--- Código/01_Basicos/Hora/Program.cs
+++ Código/01_Basicos/Hora/Program.cs
@@ -4,58 +4,47 @@
      * App principal apenas para teste da classe Hora
      */
     internal class Program {
-        static void testeBasico() {
-            Hora minhaHora = new Hora();
-
-            minhaHora.hora = 22;
-            Console.WriteLine(minhaHora.hora);
-
-            minhaHora.ajustar(20, 30, 29);  //válido e todos com dois dígitos
-            Console.WriteLine("Hora: " + minhaHora.horaFormatada());
-
-            minhaHora.ajustar(20, 30, 9);  //válido e um com um dígito                 
-            Console.WriteLine("Hora: " + minhaHora.horaFormatada());
-
-            minhaHora.ajustar(9, 8, 5);    //válido e todos com um dígito
-            Console.WriteLine("Hora: " + minhaHora.horaFormatada());
-
-            minhaHora.ajustar(20, 68, 15);      //inválido, deveria ir para 00:00:00
-            Console.WriteLine("Hora: " + minhaHora.horaFormatada());
-        }
-
-        static void testeIncremento() {
-            Hora horaBase = new Hora();
-            horaBase.ajustar(19, 54, 00);
-            Console.WriteLine("Hora original: " + horaBase.horaFormatada());
-            horaBase.incrementar(28, 's');          //19:54:28
-            Console.WriteLine("Nova hora: " + horaBase.horaFormatada());
-            horaBase.incrementar(44, 's');          //19:55:12 (72->12)
-            Console.WriteLine("Nova hora: " + horaBase.horaFormatada());
-            horaBase.incrementar(20, 'm');          //20:15:12 (75->15)
-            Console.WriteLine("Nova hora: " + horaBase.horaFormatada());
-            horaBase.incrementar(6, 'h');           //02:15:12 (26->02_
-            Console.WriteLine("Nova hora: " + horaBase.horaFormatada());
-
-            
-        }
-
-        static void testeVisibilidade() {
-            Hora horaTeste = new Hora();
-            horaTeste.ajustar(22, 04, 13);      //não podemos usar os atributos diretamente
-            Console.WriteLine("Hora original: " + horaTeste.horaFormatada());
-            horaTeste.incrementar(44, 'm');     //não podemos usar os atributos diretamente
-            Console.WriteLine("Nova hora: " + horaTeste.horaFormatada());
-        }
-
         static void Main(string[] args) {
-            testeBasico();
-            Console.Write("Enter para continuar.");
-            Console.ReadLine();
-            testeIncremento();
-            Console.Write("Enter para continuar.");
-            Console.ReadLine();
-            testeVisibilidade();
-        }
+            Hora hora;
+            Hora hora2;
+            int horas;
+            int minutos;
+            int segundos;
+            int incremento;
+            string horaDoUsuario;
 
+            Console.Write("Por favor, digite um horário válido (HH:MM:SS): ");
+            horaDoUsuario = Console.ReadLine();
+
+            string[] detalhesHora = horaDoUsuario.Split(":");
+            horas = int.Parse(detalhesHora[0]);
+            minutos = int.Parse(detalhesHora[1]);
+            segundos = int.Parse(detalhesHora[2]);
+
+            hora = new Hora();
+            hora.Ajustar(horas, minutos, segundos);
+
+            Console.Write("Sua hora escolhida: ");
+            Console.WriteLine(hora.HoraFormatada());
+
+            Console.Write("\nPor favor, digite um incremento em segundos positivos: ");
+            incremento = int.Parse(Console.ReadLine());
+
+            hora2 = hora.Incrementar(incremento);
+
+            Console.WriteLine($"{hora.HoraFormatada()} é sua hora original e {hora2.HoraFormatada()} " +
+                $"é sua hora com incremento de {incremento} segundos");
+                            
+
+            Console.Write($"{hora.HoraFormatada()} está na frente de {hora2.HoraFormatada()}? ");
+            Console.WriteLine(hora.EstahNaFrenteDe(hora2));
+
+            Console.Write($"{hora2.HoraFormatada()} está na frente de {hora.HoraFormatada()}? ");
+            Console.WriteLine(hora2.EstahNaFrenteDe(hora));
+
+            Console.Write("Ajustando a hora para 19:08:24: ");
+            hora.Ajustar(19, 8, 24);
+            Console.WriteLine(hora.HoraFormatada());
+        }
     }
 }
