7eb2a44458de30ee04653e780dc6dfe5767e920a
João Caram <caram@pucminas.br>
2024-09-06 17:22:42 -0300

    Add testes pizza e pedido

---
+++ Código/XulambsFoods_2024-2Tests/src/PedidoTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XulambsFoods_2024_2.src;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XulambsFoods_2024_2.src.Tests {
+    [TestClass()]
+    public class PedidoTests {
+        static Pedido pedido;
+        static Pizza pizzaCom2Ingredientes;
+        static Pizza pizzaSemIngredientes;
+
+        [TestInitialize]
+        public void SetUp() {
+            pizzaCom2Ingredientes = new Pizza(2);
+            pizzaSemIngredientes = new Pizza();
+            pedido = new Pedido();
+        }
+
+        [TestMethod()]
+        public void AdicionaPizzaCorretamente() {
+            Assert.AreEqual(1, pedido.Adicionar(pizzaCom2Ingredientes));
+        }
+
+        [TestMethod()]
+        public void naoAdicionaPizzaEmPedidoFechado() {
+            pedido.Adicionar(pizzaCom2Ingredientes);
+            pedido.FecharPedido();
+            Assert.AreEqual(1, pedido.Adicionar(pizzaSemIngredientes));
+        }
+
+        [TestMethod()]
+        public void calculaPrecoCorretamente() {
+            pedido.Adicionar(pizzaCom2Ingredientes);
+            pedido.Adicionar(pizzaSemIngredientes);
+            Assert.AreEqual(68d, pedido.PrecoAPagar(), 0.01);
+        }
+
+        [TestMethod()]
+        public void relatorioContemDetalhes() {
+            pedido.Adicionar(pizzaCom2Ingredientes);
+            pedido.Adicionar(pizzaSemIngredientes);
+            string relatorio = pedido.Relatorio();
+            Assert.IsTrue(relatorio.Contains("29,00"));
+            Assert.IsTrue(relatorio.Contains("39,00"));
+            Assert.IsTrue(relatorio.Contains("68,00"));
+        }
+    }
+}
---
+++ Código/XulambsFoods_2024-2Tests/src/PizzaTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XulambsFoods_2024_2.src;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XulambsFoods_2024_2.src.Tests {
+    [TestClass()]
+    public class PizzaTests {
+        static Pizza pizza;
+
+        [TestInitialize]
+        public void SetUp() {
+            pizza = new Pizza();
+        }
+
+        [TestMethod()]
+        public void CriarPizzaVazia() {
+            Assert.IsTrue(pizza.NotaDeCompra().Contains("0 ingredientes"));
+        }
+
+        [TestMethod()]
+        public void CriarPizzarComIngredientes() {
+            pizza = new Pizza(2);
+            Assert.IsTrue(pizza.NotaDeCompra().Contains("2 ingredientes"));
+        }
+
+        [TestMethod()]
+        public void CalculaValorCorretamente() {
+            pizza.AdicionarIngredientes(2);
+            Assert.AreEqual(39d, pizza.ValorFinal());
+        }
+
+        [TestMethod()]
+        public void AdicionaIngredientesCorretamente() {
+            pizza = new Pizza();
+            Assert.AreEqual(3, pizza.AdicionarIngredientes(3));
+        }
+
+        [TestMethod()]
+        public void NaoPodeAdicionarIngredientesAlemDoMaximo() {
+            pizza = new Pizza(6);
+            Assert.AreEqual(6, pizza.AdicionarIngredientes(3));
+        }
+
+        [TestMethod()]
+        public void GeraNotaDeCompraComDetalhes() {
+            pizza = new Pizza(6);
+            Assert.IsTrue(pizza.NotaDeCompra().Contains("6 ingredientes"));
+        }
+    }
+}
