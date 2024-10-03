e8ef37b0723e82e1c83b31a17a94a3406dc24eda
João Caram <caram@pucminas.br>
2024-10-03 18:00:40 -0300

    Xulambs Foods Sanduiche: esqueleto das classes

---
+++ Código/XulambsFoods/src/Comida.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+
+/** 
+    * MIT License
+    *
+    * Copyright(c) 2024 João Caram <caram@pucminas.br>
+    *
+    * Permission is hereby granted, free of charge, to any person obtaining a copy
+    * of this software and associated documentation files (the "Software"), to deal
+    * in the Software without restriction, including without limitation the rights
+    * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    * copies of the Software, and to permit persons to whom the Software is
+    * furnished to do so, subject to the following conditions:
+    *
+    * The above copyright notice and this permission notice shall be included in all
+    * copies or substantial portions of the Software.
+    *
+    * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    * SOFTWARE.
+    */
+
+namespace XulambsFoods_2024_2.src {
+    public abstract class Comida {
+        private int _maxIngredientes;
+        private string _descricao;
+        private double _precoBase;
+        private double _valorAdicional;
+        protected int _quantidadeIngredientes;
+
+        protected Comida(string desc, int maxAdicionais, double valorBase, double valorAdicional) {
+
+        }
+
+        protected double ValorAdicionais() {
+
+        }
+
+        protected bool PodeAdicionar(int quantos) {
+
+        }
+
+        public abstract double ValorFinal();
+
+        public int AdicionarIngredientes(int quantos) {
+
+        }
+
+        public string NotaDeCompra() {
+
+        }
+    }
+}
---
+++ Código/XulambsFoods/src/Sanduiche.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+
+/** 
+    * MIT License
+    *
+    * Copyright(c) 2024 João Caram <caram@pucminas.br>
+    *
+    * Permission is hereby granted, free of charge, to any person obtaining a copy
+    * of this software and associated documentation files (the "Software"), to deal
+    * in the Software without restriction, including without limitation the rights
+    * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    * copies of the Software, and to permit persons to whom the Software is
+    * furnished to do so, subject to the following conditions:
+    *
+    * The above copyright notice and this permission notice shall be included in all
+    * copies or substantial portions of the Software.
+    *
+    * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    * SOFTWARE.
+    */
+
+namespace XulambsFoods_2024_2.src {
+    public class Sanduiche : Comida {
+        private const int MaxIngredientes = 5;
+        private const string Descricao = "Sanduiche";
+        private const double PrecoBase = 15d;
+        private const double ValorAdicional = 3d;
+        private const double ValorCombo = 5;
+        private bool _comboFritas;
+
+        public Sanduiche() {
+
+        }
+
+        public Sanduiche(int quantosAdicionais) {
+
+        }
+
+        public Sanduiche(bool combo) {
+
+        }
+
+        public Sanduiche(int quantosAdicionais, bool combo) {
+
+        }
+
+        public double ValorFinal() {
+
+        }
+
+        public string NotaDeCompra() {
+
+        }
+    }
+}
