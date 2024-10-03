0a60f929f3918f524dd33d31f9f25964c1d97b12
João Caram <caram@pucminas.br>
2024-10-03 20:47:11 -0300

    Revert "Criacao das herancas de Comida"
    This reverts commit 1083610fc392c4d2b80eafcec6a7a46bc049ca8f.

--- Código/XulambsFoods/src/Comida.cs
+++ Código/XulambsFoods/src/Comida.cs
@@ -34,49 +34,25 @@ namespace XulambsFoods_2024_2.src {
         protected int _quantidadeIngredientes;
 
         protected Comida(string desc, int maxAdicionais, double valorBase, double valorAdicional) {
-            _maxIngredientes = maxAdicionais;
-            _descricao = desc;
-            _precoBase = valorBase;
-            _valorAdicional = valorAdicional;
-            _quantidadeIngredientes = 0;
+
         }
 
-        /// <summary>
-        /// Calcula o valor dos adicionais para o preço final da pizza. Atualmente o valor dos adicionais é a multiplicação da quantidade de adicionais por seu valor unitário
-        /// </summary>
-        /// <returns>Double com o valor a ser cobrado pelos adicionais.</returns>
         protected double ValorAdicionais() {
-            return _quantidadeIngredientes * _valorAdicional;
+
         }
 
-        /// <summary>
-        ///Faz a verificação de limites para adicionar ingredientes na pizza.Retorna TRUE/FALSE conforme seja possível ou não adicionar 
-        ///esta quantidade de ingredientes.
-        /// </summary>
-        /// <param name="quantos">Quantidade de ingredientes a adicionar.</param>
-        /// <returns>TRUE/FALSE conforme seja possível ou não adicionar esta quantidade de ingredientes.</returns>
         protected bool PodeAdicionar(int quantos) {
-            return (quantos > 0 && 
-                    quantos + _quantidadeIngredientes <= _maxIngredientes);
+
         }
 
         public abstract double ValorFinal();
 
-        /// <summary>
-        /// Tenta adicionar ingredientes na pizza.Caso a adição seja inválida(ultrapassando limites ou com valores negativos), mantém
-        /// a quantidade atual de ingredientes.Retorna a quantidade de ingredientes após a execução do método.
-        /// </summary>
-        /// <param name="quantos">Quantos ingredientes a serem adicionados (>0)</param>
-        /// <returns>Quantos ingredientes a pizza tem após a execução</returns>
         public int AdicionarIngredientes(int quantos) {
-            if (PodeAdicionar(quantos)) {
-                _quantidadeIngredientes += quantos;
-            }
-            return _quantidadeIngredientes;
+
         }
 
-        public virtual string NotaDeCompra() {
-            return $"{_descricao} ({_precoBase:C2}) com {_quantidadeIngredientes} ingredientes ({ValorAdicionais():C2}).";
+        public string NotaDeCompra() {
+
         }
     }
 }
--- Código/XulambsFoods/src/Sanduiche.cs
+++ Código/XulambsFoods/src/Sanduiche.cs
@@ -34,42 +34,28 @@ namespace XulambsFoods_2024_2.src {
         private const double ValorCombo = 5;
         private bool _comboFritas;
 
-        private void init(int quantosAdicionais, bool combo) {
-            AdicionarIngredientes(quantosAdicionais);
-            _comboFritas = combo;
-        }
-        public Sanduiche(): base(Descricao, MaxIngredientes, PrecoBase, ValorAdicional) {
-            init(0, false);
+        public Sanduiche() {
+
         }
 
-        public Sanduiche(int quantosAdicionais) : 
-            base(Descricao, MaxIngredientes, PrecoBase, ValorAdicional) {
-            init(quantosAdicionais, false);
+        public Sanduiche(int quantosAdicionais) {
+
         }
 
-        public Sanduiche(bool combo):
-            base(Descricao, MaxIngredientes, PrecoBase, ValorAdicional) {
-            init(0, combo);
+        public Sanduiche(bool combo) {
+
         }
 
-        public Sanduiche(int quantosAdicionais, bool combo):
-            base(Descricao, MaxIngredientes, PrecoBase, ValorAdicional) {
-            init(quantosAdicionais, combo);
+        public Sanduiche(int quantosAdicionais, bool combo) {
+
         }
 
-        public override double ValorFinal() {
-            double valor = PrecoBase + ValorAdicionais();
-            if (_comboFritas)
-                 valor += ValorCombo;
-            return valor;
+        public double ValorFinal() {
+
         }
 
-        public override string NotaDeCompra() {
-            string notinha = base.NotaDeCompra();
-            if(_comboFritas)
-                notinha += $"\n\tCombo com fritas: {ValorCombo:C2}";
-            notinha += $"\nValor a pagar: {ValorFinal():C2}";
-            return notinha;
+        public string NotaDeCompra() {
+
         }
     }
 }
