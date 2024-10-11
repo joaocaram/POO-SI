85fbf19e904f73c083f36362832a0a7a44bb73e1
João Caram <caram@pucminas.br>
2024-10-11 11:03:29 -0300

    Renomeação

---
+++ Código/XulambsFoods/src/IPedido.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XulambsFoods_2024_2.src {
+    public interface IPedido {
+        public bool PodeAdicionar();
+        public double ValorTaxa();
+        public string Relatorio();
+    }
+}
