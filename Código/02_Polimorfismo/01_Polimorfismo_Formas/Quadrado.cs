52ccd57e23801a600a64f37f6ae7faf587d46374
João Caram <caram@pucminas.br>
2024-11-14 17:21:07 -0300

    Criação da classe Quadrado

---
+++ Código/02_Polimorfismo/01_Polimorfismo_Formas/Quadrado.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoliFiguras {
+    internal class Quadrado : PoligonoReto {
+
+        public Quadrado(double lado) : base("Quadrado", lado, lado) {
+
+        }
+
+        public override double area() {
+            return Math.Pow(_basePoligono, 2.0);
+        }
+
+        public override double perimetro() {
+            return 4 * _basePoligono;
+        }
+
+        public override string ToString() {
+            return $"{base.ToString()}  | Lado: {_basePoligono:F2}";
+        }
+    }
+}
