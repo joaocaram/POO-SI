b176f379a9cdf468b352dd53a65beefee4d4402f
Joao <caram@pucminas.br>
2024-11-19 19:08:14 -0300

    Poligono Reto

---
+++ Código/02_Polimorfismo/01_Polimorfismo_Formas/PoligonoReto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoliFiguras {
+    internal abstract class PoligonoReto : FormaGeometrica {
+       
+        protected double _basePoligono;
+        protected double _alturaPoligono;
+
+        protected PoligonoReto(string desc, double b, double h): base(desc) {
+            _basePoligono = _alturaPoligono = 0.1;
+            if (b > 0.1)
+                _basePoligono = b;
+            if (h > 0.1)
+                _alturaPoligono = h;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            try { 
+            PoligonoReto outro = (PoligonoReto)obj;
+            return (this._basePoligono == outro._basePoligono
+                    && this._alturaPoligono == outro._alturaPoligono
+                    && this._descricao.Equals(outro._descricao));
+            }
+            catch(InvalidCastException ic)
+            {
+                return false;
+            }
+         }
+
+    }
+}
