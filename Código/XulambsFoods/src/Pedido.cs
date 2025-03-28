9edb31485a2ef02498d0e84a72fc096426ce176e
João Caram <caram@pucminas.br>
2025-03-28 15:57:00 -0300

    Refatoração/herança: Pedido/PedidoEntrega

--- Código/XulambsFoods/src/Pedido.cs
+++ Código/XulambsFoods/src/Pedido.cs
@@ -4,30 +4,135 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+    /** 
+    * MIT License
+    *
+    * Copyright(c) 2024-25 João Caram <caram@pucminas.br>
+    *
+    * Permission is hereby granted, free of charge, to any person obtaining a copy
+    * of this software and associated documentation files (the "Software"), to deal
+    * in the Software without restriction, including without limitation the rights
+    * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    * copies of the Software, and to permit persons to whom the Software is
+    * furnished to do so, subject to the following conditions:
+    *
+    * The above copyright notice and this permission notice shall be included in all
+    * copies or substantial portions of the Software.
+    *
+    * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    * SOFTWARE.
+    */
+
 namespace XulambsFoods_2025_1.src {
+    /// <summary>
+    /// Um pedido pode agrupar várias pizzas. Deve exibir um relatório descritivo 
+    /// com o detalhamento das pizzas e o valor total a pagar.
+    /// </summary>
     public class Pedido {
+        /// <summary>
+        /// Static para geração do id do pedido seguinte.
+        /// </summary>
         private static int s_ultimoPedido = 0;
+        /// <summary>
+        /// Apenas para controle do vetor de pizzas.
+        /// </summary>
         private const int MaxPizzas = 100;
 
+        /// <summary>
+        /// Máximo de pizzas permitidas(pelo controle de tamanho)
+        /// </summary>
+        private int _maxPizzas;
+        
+        /// <summary>
+        /// Identificador do pedido (gerado automaticamente) 
+        /// </summary>
         private int _idPedido;
+
+        /// <summary>
+        /// Data do pedido, para uso futuro
+        /// </summary>
         private DateOnly _data;
+
+        /// <summary>
+        /// Armazenando as pizzas em um vetor. Pode ser melhorado futuramente.
+        /// </summary>
         private Pizza[] _pizzas;
+
+        /// <summary>
+        /// Quantidade de pizzas no pedido atualmente.
+        /// </summary>
         private int _quantPizzas;
+
+        /// <summary>
+        /// Estado do pedido: aberto pode ser modificado, fechado não pode.
+        /// </summary>
         private bool _aberto;
 
-        public Pedido() {
+        /// <summary>
+        /// Inicializador privado. Gera o identificador, cria o vetor
+        /// e inicializa demais atributos
+        /// </summary>
+        /// <param name="maxPizzas"></param>
+        private void init(int maxPizzas) {
             s_ultimoPedido++;
             _idPedido = s_ultimoPedido;
+            _maxPizzas = maxPizzas;
             _data = DateOnly.FromDateTime(DateTime.Now);
-            _pizzas = new Pizza[MaxPizzas];
+            if (_maxPizzas < 1)
+                _maxPizzas = 1;
+            _pizzas = new Pizza[_maxPizzas];
             _quantPizzas = 0;
             _aberto = true;
         }
 
-        private bool PodeAdicionar() {
-            return _aberto;
+        /// <summary>
+        /// Construtor para uso de classes derivadas, permitindo
+        /// restringir a quantidade de pizzas armazenadas.
+        /// </summary>
+        /// <param name="maxPizzas">Máximo de pizzas no pedido. Deve ser >= 1.</param>
+        protected Pedido(int maxPizzas) {
+            init(maxPizzas);
         }
 
+        /// <summary>
+        /// Cria um pedido vazio, com a data de hoje e identificador gerado
+        /// automaticamente.
+        /// </summary>
+        public Pedido() {
+            init(MaxPizzas);
+        }
+               
+        /// <summary>
+        /// Verifica se ainda é possível adicionar pizzas ao pedido, por seu estado
+        /// e quantidade de pizzas.
+        /// </summary>
+        /// <returns>TRUE/FALSE conforme seja permitido adicionar ou não</returns>
+        private bool PodeAdicionar() {
+            return _aberto && _quantPizzas < _maxPizzas;
+        }
+
+        /// <summary>
+        /// Encapsula a lógica do cálculo do valor dos itens do pedidos (soma dos valores)
+        /// </summary>
+        /// <returns>Valor dos itens do pedido (soma dos valores dos itens)</returns>
+        protected double ValorItens() {
+            double preco = 0d;
+            for (int i = 0; i < _quantPizzas; i++) {
+                preco += _pizzas[i].ValorFinal();
+            }
+            return preco;
+        }
+
+        /// <summary>
+        /// Tenta adicionar uma pizza ao pedido. Se não for possível, ignora a operação.
+        /// </summary>
+        /// <param name="pizza">A pizza a ser incluída no pedido</param>
+        /// <returns>Quantidade de pizzas no pedido após a execução.</returns>
         public int Adicionar(Pizza pizza) {
             if (PodeAdicionar()) {
                 _pizzas[_quantPizzas] = pizza;
@@ -36,26 +141,56 @@ namespace XulambsFoods_2025_1.src {
             return _quantPizzas;
         }
 
+        /// <summary>
+        /// Fecha o pedido, caso ele tenha pelo menos 1 pizza. Caso contrário,
+        /// ignora a operação.
+        /// </summary>
         public void FecharPedido() {
-            _aberto = false;
+            if(_quantPizzas > 0)
+                _aberto = false;
         }
 
-        public double PrecoAPagar() {
-            double preco = 0d;
-            for (int i = 0; i < _quantPizzas; i++) {
-                preco += _pizzas[i].ValorFinal();
-            }
-            return preco;
+        /// <summary>
+        /// Retorna o preço a pagar por um pedido (valor double positivo).
+        /// </summary>
+        /// <returns>Double com o valor a pagar pelo pedido.</returns>
+        public virtual double PrecoAPagar() {
+            return ValorItens();
         }
 
-        public string Relatorio() {
-            StringBuilder relat = new StringBuilder($"Pedido {_idPedido} - {_data}\n");
+        /// <summary>
+        /// Detalhamento do pedido (id, data, detalhes das pizzas)
+        /// comum a todos os pedidos.
+        /// </summary>
+        /// <returns>String multilinha com as informações acima</returns>
+        protected string DetalhamentoPedido(){
+            StringBuilder relat = new StringBuilder($"nº{ _idPedido} - { _data}\n");
             relat.AppendLine("==============================");
-            for (int i = 0; i < _quantPizzas; i++) {
+            for (int i = 0; i < _quantPizzas; i++)
+            {
                 relat.AppendLine(_pizzas[i].NotaDeCompra());
             }
+            return relat.ToString();
+        }
+
+        /// <summary>
+        /// Relatório do pedido, contendo seu identificador, data,
+        /// detalhamento das pizzas e preço a pagar.
+        /// </summary>
+        /// <returns>Uma string, multilinhas, com a informação descrita</returns>
+        public virtual string Relatorio() {
+            StringBuilder relat = new StringBuilder($"Pedido Local {DetalhamentoPedido()}");
             relat.Append($"Valor a pagar: {PrecoAPagar():C2}");
             return relat.ToString();
+        }
+
+        /// <summary>
+        /// Retorna o id do pedido, para fins de comparação 
+        /// (faremos melhor que isso em breve)
+        /// </summary>
+        /// <returns>Identificador do pedido (inteiro positivo)</returns>
+        public int GetID() {
+            return _idPedido;
         }
     }
 }
---
+++ Código/XulambsFoods/src/PedidoEntrega.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/** 
+    * MIT License
+    *
+    * Copyright(c) 2024-25 João Caram <caram@pucminas.br>
+    *
+    * Permission is hereby granted, free of charge, to any person obtaining a copy
+    * of this software and associated documentation files (the "Software"), to deal
+    * in the Software without restriction, including without limitation the rights
+    * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    * copies of the Software, and to permit persons to whom the Software is
+    * furnished to do so, subject to the following conditions:
+    *
+    * The above copyright notice and this permission notice shall be included in all
+    * copies or substantial portions of the Software.
+    *
+    * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    * SOFTWARE.
+    */
+
+namespace XulambsFoods_2025_1.src {
+        
+    /// <summary>
+    /// Pedido para entrega: há limite de quantidade de pizzas e há uma taxa de
+    /// entrega, que pode ser de valor 0.
+    /// </summary>
+	public class PedidoEntrega : Pedido {
+        /// <summary>
+        /// Constante: máximo de pizzas em um pedido para entrega
+        /// </summary>
+        private const int MaxEntrega = 8;
+        
+        /// <summary>
+        /// Vetor 'constante' com os valores da taxa de entrega.
+        /// Pergunta: isso pode ser melhor?
+        /// </summary>
+        private readonly double[] TaxasEntrega = [ 0, 5, 8 ];
+        
+        /// <summary>
+        /// Vetor 'constante' com as distâncias de entrega correspondentes
+        /// às taxas. Pergunta: isso pode ser melhor?
+        /// </summary>
+        private readonly double[] DistanciasEntrega = [4, 8, Double.MaxValue];
+       
+        /// <summary>
+        /// Distância da entrega. Deve ser maior que 0.
+        /// </summary>
+        private double _distanciaEntrega;
+
+        /// <summary>
+        /// Construtor do pedido para entrega. Recebe uma distância, que
+        /// deve ser um valor positivo.
+        /// </summary>
+        /// <param name="distancia">Distância da entrega (double > 0)</param>
+        public PedidoEntrega(double distancia): base(MaxEntrega) {
+            if (distancia < 0.1)
+                distancia = 0.1;
+            _distanciaEntrega = distancia;
+        }
+
+        /// <summary>
+        /// Calcula o valor da taxa de acordo com a distância de entrega
+        /// do pedido. O valor é um double não negativo.
+        /// </summary>
+        /// <returns>Valor da taxa de entrega (double não negativo)</returns>
+        private double ValorTaxa() {
+            int i = 0;
+            while (_distanciaEntrega > DistanciasEntrega[i])
+                i++;
+            return TaxasEntrega[i];
+        }
+
+        /// <summary>
+        /// Preço a pagar pelo pedido de entrega: valor de um pedido +
+        /// o valor da taxa de entrega.
+        /// </summary>
+        /// <returns>Valor a ser pago pelo pedido. (double positivo)</returns>
+        public override double PrecoAPagar() {
+            return base.PrecoAPagar() + ValorTaxa();
+        }
+
+        /// <summary>
+        /// Relatório do pedido, contendo identificador, data, distância,
+        /// detalhamento das pizzas, valor da taxa de entrega e valor do pedido.
+        /// </summary>
+        /// <returns>String multilinhas com o detalhamento descrito do pedido.</returns>
+        public override string Relatorio() {
+            StringBuilder relat = new StringBuilder($"Pedido para Entrega ({_distanciaEntrega:F1}km) {DetalhamentoPedido()}");
+            
+            relat.AppendLine($"\nValor dos itens: {ValorItens():C2}");
+            relat.AppendLine($"Taxa de entrega: {ValorTaxa():C2}");
+            relat.Append($"Valor a pagar: {PrecoAPagar():C2}");
+            return relat.ToString();
+        }
+    }
+}
+
+
