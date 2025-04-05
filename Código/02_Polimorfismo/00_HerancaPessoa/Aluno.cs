0fb2ffeabb3c5c1843dd1fd42ddbb829d0514b0e
João Caram <caram@pucminas.br>
2025-04-05 15:46:22 -0300

    Adequação Aluno a padrões C# e documentação

--- Código/02_Polimorfismo/00_HerancaPessoa/Aluno.cs
+++ Código/02_Polimorfismo/00_HerancaPessoa/Aluno.cs
@@ -11,20 +11,44 @@ namespace HerancaPessoa
     {
 
 
-    private static double horaAula;
-    private double valorMatricula;
+        private static double s_horaAula;
+        private double _valorMatricula;
 
-    public Aluno(string nome, DateOnly nascimento, string documento):
-            base(nome, nascimento, documento)
-    {
+        static Aluno()
+        {
+            s_horaAula = 25d;
+        }
+
+        /// <summary>
+        /// Construtor de um aluno, que recebe nome, data de nascimento,
+        /// documento e um email de contato. O aluno também recebe o valor
+        /// pago como matrícula (mínimo de R$1000)
+        /// </summary>
+        /// <param name="nome">Nome da pessoa</param>
+        /// <param name="nascimento">Data de nascimento</param>
+        /// <param name="documento">Um documento identificador</param>
+        /// <param name="email">O email de contato da pessoa</param>
+        /// <param name="valorMatricula">Matrícula paga (>=1000)</param>
+        public Aluno(string nome, DateOnly nascimento, string documento, string email,
+                        double valorMatricula) :
+                base(nome, nascimento, documento, email)
+        {
+            if (valorMatricula < 1000d)
+                valorMatricula = 1000d;
+            _valorMatricula = valorMatricula;
+        }
+
+        /// <summary>
+        /// Valor da mensalidade do aluno: sua carga horária total do semestre,
+        /// multiplicado pelo valor da hora aula, descontando-se o valor pago
+        /// como matrícula - e tudo isso dividido por 6 meses.
+        /// </summary>
+        /// <returns>Double com o valor da mensalidade</returns>
+        public double ValorMensalidade()
+        {
+            return ((_cargaHoraria * s_horaAula) - _valorMatricula) / 6.0;
+        }
 
     }
-
-    public double valorMensalidade()
-    {
-            return ((cargaHoraria * horaAula) - valorMatricula) / 6.0;
-    }
-
-}
 
 }
