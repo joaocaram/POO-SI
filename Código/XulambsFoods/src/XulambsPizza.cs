c1e8138f904362c7495f92bdae05b226aa8c11f8
João Caram <caram@pucminas.br>
2025-10-16 17:51:30 -0300

    Mesclagem do App

--- Código/XulambsFoods/src/XulambsPizza.cs
+++ Código/XulambsFoods/src/XulambsPizza.cs
@@ -1,291 +1,255 @@
-namespace XulambsFoods_2025_1.src
-{
-
-using System.Linq;
-
 namespace XulambsFoods_2025_1.src {
 
-    /** 
-    * MIT License
-    *
-    * Copyright(c) 2025 João Caram <caram@pucminas.br>
-    *
-    * Permission is hereby granted, free of charge, to any person obtaining a copy
-    * of this software and associated documentation files (the "Software"), to deal
-    * in the Software without restriction, including without limitation the rights
-    * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    * copies of the Software, and to permit persons to whom the Software is
-    * furnished to do so, subject to the following conditions:
-    *
-    * The above copyright notice and this permission notice shall be included in all
-    * copies or substantial portions of the Software.
-    *
-    * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    * SOFTWARE.
-    */
+    using System.Linq;
 
-    public class XulambsPizza
-    {
+    namespace XulambsFoods_2025_1.src {
 
-        static LinkedList<Pedido> pedidos = new LinkedList<Pedido>();
+        /** 
+        * MIT License
+        *
+        * Copyright(c) 2025 João Caram <caram@pucminas.br>
+        *
+        * Permission is hereby granted, free of charge, to any person obtaining a copy
+        * of this software and associated documentation files (the "Software"), to deal
+        * in the Software without restriction, including without limitation the rights
+        * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        * copies of the Software, and to permit persons to whom the Software is
+        * furnished to do so, subject to the following conditions:
+        *
+        * The above copyright notice and this permission notice shall be included in all
+        * copies or substantial portions of the Software.
+        *
+        * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        * SOFTWARE.
+        */
 
-        static void Cabecalho()
-        {
-            Console.Clear();
-            Console.WriteLine("XULAMBS PIZZA v0.4\n================");
-        }
+        public class XulambsPizza {
 
-        static void Pausa()
-        {
-            Console.WriteLine("Digite enter para continuar...");
-            Console.ReadLine();
-        }
+            static LinkedList<Pedido> pedidos = new LinkedList<Pedido>();
 
-        static int ExibirMenuPrincipal()
-        {
-            Cabecalho();
-            Console.WriteLine("1 - Abrir Pedido");
-            Console.WriteLine("2 - Alterar Pedido");
-            Console.WriteLine("3 - Relatório de Pedido");
-            Console.WriteLine("4 - Fechar Pedido");
-            Console.WriteLine("5 - Valor do último pedido");
-            Console.WriteLine("0 - Finalizar");
-            Console.Write("Digite sua escolha: ");
-            return int.Parse(Console.ReadLine());
-        }
-
-        static void AdicionarPizzas(Pedido pedido)
-        {
-            string conf;
-
-            do
-            {
-                Pizza novaPizza = ComprarPizza();
-                pedido.Adicionar(novaPizza);
-                Console.Write("\nQuer uma nova pizza (S/N)? ");
-                conf = Console.ReadLine().ToUpper();
-            } while (conf.Equals("S"));
-        }
-
-        static Pedido AbrirPedido()
-        {
-            Pedido novoPedido = EscolherTipoPedido();
-            AdicionarPizzas(novoPedido);
-            MostrarPedido(novoPedido);
-            return novoPedido;
-        }
-
-        static Pedido EscolherTipoPedido()
-        {
-            int opcao = ExibirMenuTipoPedido();
-            return opcao switch
-            {
-                2 => CriarPedidoEntrega(),
-                1 or _ => CriarPedidoLocal()
-            };
-        }
-
-        static int ExibirMenuTipoPedido()
-        {
-            Cabecalho();
-            Console.WriteLine("Escolha o tipo de pedido:");
-            Console.WriteLine("1 - Local (padrão)");
-            Console.WriteLine("2 - Pedido para entrega");
-            Console.Write("Sua opção: ");
-            return int.Parse(Console.ReadLine());
-        }
-
-        static Pedido CriarPedidoLocal()
-        {
-            return new PedidoLocal();
-        }
-
-        static Pedido CriarPedidoEntrega()
-        {
-            Console.WriteLine("Pedido para Entrega.");
-            Console.Write("Distância: ");
-            double dist = double.Parse(Console.ReadLine());
-            return new PedidoEntrega(dist);
-        }
-
-        static int ExibirMenuIngredientes(Pizza pizza)
-        {
-            Cabecalho();
-            Console.WriteLine("Personalizar a Pizza\n");
-            MostrarNota(pizza);
-            Console.WriteLine("\n1 - Acrescentar ingredientes");
-            Console.WriteLine("0 - Não quero alterar");
-            Console.Write("Digite sua escolha: ");
-            return int.Parse(Console.ReadLine());
-        }
-
-        static int ExibirMenuBordas() {
-            Cabecalho();
-            int i = 1;
-            Console.WriteLine("Escolha sua borda: ");
-            foreach(string nome in Enum.GetNames<EBorda>()) {
-                Console.WriteLine($"{i} - {nome}");
-                i++;
+            static void Cabecalho() {
+                Console.Clear();
+                Console.WriteLine("XULAMBS PIZZA v0.4\n================");
             }
-            Console.Write("Digite sua escolha: ");
-            return int.Parse(Console.ReadLine());
-        }
 
-        static void EscolherBorda(Pizza pizza) {
-            int opcao = ExibirMenuBordas();
-            EBorda borda = Enum.GetValues<EBorda>().ElementAt(opcao - 1);
-            pizza.AdicionarBorda(borda);
-        }
-
-        static Pizza ComprarPizza() {
-            Cabecalho();
-            Console.WriteLine("Comprando uma nova pizza:");
-            Pizza novaPizza = new Pizza();
-            EscolherBorda(novaPizza);
-            EscolherIngredientes(novaPizza);
-            Console.WriteLine();
-            MostrarNota(novaPizza);
-            return novaPizza;
-        }
-
-        static void EscolherIngredientes(Pizza pizza)
-        {
-            int opcao = ExibirMenuIngredientes(pizza);
-            if (opcao == 1)
-            {
-                Console.Write("Quantos ingredientes? ");
-                int adicionais = int.Parse(Console.ReadLine());
-                pizza.AdicionarIngredientes(adicionais);
+            static void Pausa() {
+                Console.WriteLine("Digite enter para continuar...");
+                Console.ReadLine();
             }
-        }
 
-        static void EscolherBorda(Pizza pizza)
-        {
-            int opcao = ExibirMenuBorda();
-            EBorda bordaEscolhida = Enum.GetValues<EBorda>()[opcao - 1];
-            pizza.AdicionarBorda(bordaEscolhida);
-        }
-
-        static int ExibirMenuBorda()
-        {
-            Cabecalho();
-            Console.WriteLine("Escolha a borda da pizza:");
-            int i = 1;
-            foreach (string nomeBorda in Enum.GetNames<EBorda>())
-            {
-                Console.WriteLine($"{i} - {nomeBorda}");
-                i++;
+            static int ExibirMenuPrincipal() {
+                Cabecalho();
+                Console.WriteLine("1 - Abrir Pedido");
+                Console.WriteLine("2 - Alterar Pedido");
+                Console.WriteLine("3 - Relatório de Pedido");
+                Console.WriteLine("4 - Fechar Pedido");
+                Console.WriteLine("5 - Valor do último pedido");
+                Console.WriteLine("0 - Finalizar");
+                Console.Write("Digite sua escolha: ");
+                return int.Parse(Console.ReadLine());
             }
-            Console.Write("Digite sua escolha: ");
-            return int.Parse(Console.ReadLine());
-        }
 
-        static void MostrarNota(Pizza pizza)
-        {
-            Console.WriteLine("Comprando: ");
-            Console.WriteLine(pizza);
-        }
+            static void AdicionarPizzas(Pedido pedido) {
+                string conf;
 
-        static void MostrarPedido(Pedido pedido)
-        {
-            Cabecalho();
-            Console.WriteLine(pedido);
-        }
-
-        static void ArmazenarPedido(Pedido pedido)
-        {
-            pedidos.AddLast(pedido);
-        }
-
-        static Pedido LocalizarPedido()
-        {
-            Cabecalho();
-            Console.WriteLine("Localizando um pedido");
-            Console.Write("Digite o número do pedido: ");
-            int numero = int.Parse(Console.ReadLine());
-            foreach (Pedido ped in pedidos)
-            {
-                if (ped.GetHashCode() == numero)
-                    return ped;
+                do {
+                    Pizza novaPizza = ComprarPizza();
+                    pedido.Adicionar(novaPizza);
+                    Console.Write("\nQuer uma nova pizza (S/N)? ");
+                    conf = Console.ReadLine().ToUpper();
+                } while (conf.Equals("S"));
             }
-            return null;
-        }
 
-        static void AlterarPedido()
-        {
-            Pedido pedidoParaAlteracao = LocalizarPedido();
-            if (pedidoParaAlteracao != null)
-            {
-                AdicionarPizzas(pedidoParaAlteracao);
-                MostrarPedido(pedidoParaAlteracao);
+            static Pedido AbrirPedido() {
+                Pedido novoPedido = EscolherTipoPedido();
+                AdicionarPizzas(novoPedido);
+                MostrarPedido(novoPedido);
+                return novoPedido;
             }
-            else
-                Console.WriteLine("Pedido não encontrado");
-        }
 
-        static void FecharPedido()
-        {
-            Pedido pedidoParaFechar = LocalizarPedido();
-            if (pedidoParaFechar != null)
-            {
-                pedidoParaFechar.FecharPedido();
-                MostrarPedido(pedidoParaFechar);
+            static Pedido EscolherTipoPedido() {
+                int opcao = ExibirMenuTipoPedido();
+                return opcao switch {
+                    2 => CriarPedidoEntrega(),
+                    1 or _ => CriarPedidoLocal()
+                };
             }
-            else
-                Console.WriteLine("Pedido não encontrado");
-        }
 
-        static void RelatorioDePedido()
-        {
-            Pedido localizado = LocalizarPedido();
-            if (localizado != null)
-                MostrarPedido(localizado);
-            else
-                Console.WriteLine("Pedido não existente");
-        }
+            static int ExibirMenuTipoPedido() {
+                Cabecalho();
+                Console.WriteLine("Escolha o tipo de pedido:");
+                Console.WriteLine("1 - Local (padrão)");
+                Console.WriteLine("2 - Pedido para entrega");
+                Console.Write("Sua opção: ");
+                return int.Parse(Console.ReadLine());
+            }
 
-        static void ValorUltimoPedido()
-        {
-            Cabecalho();
-            Pedido ultimo = pedidos.Last();
-            Console.WriteLine($"Valor do último pedido: {ultimo.PrecoAPagar():C2}");
-        }
+            static Pedido CriarPedidoLocal() {
+                return new PedidoLocal();
+            }
 
-        static void Main(string[] args)
-        {
-            int opcao = -1;
-            do
-            {
-                opcao = ExibirMenuPrincipal();
-                switch (opcao)
-                {
-                    case 1:
-                        Pedido novoPedido = AbrirPedido();
-                        ArmazenarPedido(novoPedido);
-                        break;
-                    case 2:
-                        AlterarPedido();
-                        break;
-                    case 3:
-                        RelatorioDePedido();
-                        break;
-                    case 4:
-                        FecharPedido();
-                        break;
-                    case 5:
-                        ValorUltimoPedido();
-                        break;
-                    case 0:
-                        Console.WriteLine("FLW VLW OBG VLT SMP.");
-                        break;
+            static Pedido CriarPedidoEntrega() {
+                Console.WriteLine("Pedido para Entrega.");
+                Console.Write("Distância: ");
+                double dist = double.Parse(Console.ReadLine());
+                return new PedidoEntrega(dist);
+            }
+
+            static int ExibirMenuIngredientes(Pizza pizza) {
+                Cabecalho();
+                Console.WriteLine("Personalizar a Pizza\n");
+                MostrarNota(pizza);
+                Console.WriteLine("\n1 - Acrescentar ingredientes");
+                Console.WriteLine("0 - Não quero alterar");
+                Console.Write("Digite sua escolha: ");
+                return int.Parse(Console.ReadLine());
+            }
+
+            static int ExibirMenuBordas() {
+                Cabecalho();
+                int i = 1;
+                Console.WriteLine("Escolha sua borda: ");
+                foreach (string nome in Enum.GetNames<EBorda>()) {
+                    Console.WriteLine($"{i} - {nome}");
+                    i++;
                 }
-                Pausa();
-            } while (opcao != 0);
+                Console.Write("Digite sua escolha: ");
+                return int.Parse(Console.ReadLine());
+            }
+
+            static void EscolherBorda(Pizza pizza) {
+                int opcao = ExibirMenuBordas();
+                EBorda borda = Enum.GetValues<EBorda>().ElementAt(opcao - 1);
+                pizza.AdicionarBorda(borda);
+            }
+
+            static Pizza ComprarPizza() {
+                Cabecalho();
+                Console.WriteLine("Comprando uma nova pizza:");
+                Pizza novaPizza = new Pizza();
+                EscolherBorda(novaPizza);
+                EscolherIngredientes(novaPizza);
+                Console.WriteLine();
+                MostrarNota(novaPizza);
+                return novaPizza;
+            }
+
+            static void EscolherIngredientes(Pizza pizza) {
+                int opcao = ExibirMenuIngredientes(pizza);
+                if (opcao == 1) {
+                    Console.Write("Quantos ingredientes? ");
+                    int adicionais = int.Parse(Console.ReadLine());
+                    pizza.AdicionarIngredientes(adicionais);
+                }
+            }
+
+
+            static int ExibirMenuBorda() {
+                Cabecalho();
+                Console.WriteLine("Escolha a borda da pizza:");
+                int i = 1;
+                foreach (string nomeBorda in Enum.GetNames<EBorda>()) {
+                    Console.WriteLine($"{i} - {nomeBorda}");
+                    i++;
+                }
+                Console.Write("Digite sua escolha: ");
+                return int.Parse(Console.ReadLine());
+            }
+
+            static void MostrarNota(Pizza pizza) {
+                Console.WriteLine("Comprando: ");
+                Console.WriteLine(pizza);
+            }
+
+            static void MostrarPedido(Pedido pedido) {
+                Cabecalho();
+                Console.WriteLine(pedido);
+            }
+
+            static void ArmazenarPedido(Pedido pedido) {
+                pedidos.AddLast(pedido);
+            }
+
+            static Pedido LocalizarPedido() {
+                Cabecalho();
+                Console.WriteLine("Localizando um pedido");
+                Console.Write("Digite o número do pedido: ");
+                int numero = int.Parse(Console.ReadLine());
+                foreach (Pedido ped in pedidos) {
+                    if (ped.GetHashCode() == numero)
+                        return ped;
+                }
+                return null;
+            }
+
+            static void AlterarPedido() {
+                Pedido pedidoParaAlteracao = LocalizarPedido();
+                if (pedidoParaAlteracao != null) {
+                    AdicionarPizzas(pedidoParaAlteracao);
+                    MostrarPedido(pedidoParaAlteracao);
+                }
+                else
+                    Console.WriteLine("Pedido não encontrado");
+            }
+
+            static void FecharPedido() {
+                Pedido pedidoParaFechar = LocalizarPedido();
+                if (pedidoParaFechar != null) {
+                    pedidoParaFechar.FecharPedido();
+                    MostrarPedido(pedidoParaFechar);
+                }
+                else
+                    Console.WriteLine("Pedido não encontrado");
+            }
+
+            static void RelatorioDePedido() {
+                Pedido localizado = LocalizarPedido();
+                if (localizado != null)
+                    MostrarPedido(localizado);
+                else
+                    Console.WriteLine("Pedido não existente");
+            }
+
+            static void ValorUltimoPedido() {
+                Cabecalho();
+                Pedido ultimo = pedidos.Last();
+                Console.WriteLine($"Valor do último pedido: {ultimo.PrecoAPagar():C2}");
+            }
+
+            static void Main(string[] args) {
+                int opcao = -1;
+                do {
+                    opcao = ExibirMenuPrincipal();
+                    switch (opcao) {
+                        case 1:
+                            Pedido novoPedido = AbrirPedido();
+                            ArmazenarPedido(novoPedido);
+                            break;
+                        case 2:
+                            AlterarPedido();
+                            break;
+                        case 3:
+                            RelatorioDePedido();
+                            break;
+                        case 4:
+                            FecharPedido();
+                            break;
+                        case 5:
+                            ValorUltimoPedido();
+                            break;
+                        case 0:
+                            Console.WriteLine("FLW VLW OBG VLT SMP.");
+                            break;
+                    }
+                    Pausa();
+                } while (opcao != 0);
+            }
         }
     }
+
 }
