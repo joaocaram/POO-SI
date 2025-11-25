ba9335a3c4475232ffb033df040a47c6123913db
João Caram <caram@pucminas.br>
2025-11-25 02:04:06 -0300

    Pedido com desconto

--- Código/XulambsFoods/src/Pedido.cs
+++ Código/XulambsFoods/src/Pedido.cs
@@ -24,14 +24,15 @@
 * SOFTWARE.
 */
 
-namespace XulambsFoods_2025_1.src
-{
+namespace XulambsFoods_2025_1.src {
     /// <summary>
     /// Um pedido pode agrupar várias pizzas. Deve exibir um relatório descritivo 
     /// com o detalhamento das pizzas e o valor total a pagar.
     /// </summary>
-    public abstract class Pedido
-    {
+    public abstract class Pedido {
+        static DateTime dataBase = new DateTime(2025, 02, 01, 12, 0, 0);
+        static Random sorteio = new Random(42);
+
         /// <summary>
         /// Static para geração do id do pedido seguinte.
         /// </summary>
@@ -46,26 +47,52 @@ namespace XulambsFoods_2025_1.src
         /// Data do pedido, para uso futuro
         /// </summary>
         private DateOnly _data;
-        /// <summary>
-        /// Armazenando as pizzas em um vetor. Pode ser melhorado futuramente.
-        /// </summary>
-        protected LinkedList<IProduto> _itens;
 
         /// <summary>
         /// Estado do pedido: aberto pode ser modificado, fechado não pode.
         /// </summary>
         private bool _aberto;
+
+        /// <summary>
+        /// Valor de um possível desconto para o pedido. Só pode ser aplicado o desconto uma vez.
+        /// </summary>
+        private double _desconto;
+
+        /// <summary>
+        /// Armazenando as comidas em uma lista encadeada (dinâmica). 
+        /// </summary>
+        protected LinkedList<IProduto> _itens;
+
+
+        /// <summary>
+        /// Inicializador privado. Gera o identificador, cria o vetor
+        /// e inicializa demais atributos
+        /// </summary>
+        /// <param name="maxPizzas"></param>
+        private void init(DateOnly? data) {
+            s_ultimoPedido++;
+            _idPedido = s_ultimoPedido;
+
+            if (data != null)
+                _data = (DateOnly)data;
+            else {
+                dataBase = dataBase.AddMinutes(sorteio.NextInt64(12, 50) + 1);
+                if (dataBase.Hour < 12)
+                    dataBase.AddHours(13 - dataBase.Hour);
+
+                _data = DateOnly.FromDateTime(dataBase);
+            }
+            _itens = new LinkedList<IProduto>();
+            _desconto = 0d;
+            _aberto = true;
+        }
+
         /// <summary>
         /// Cria um pedido vazio, com a data de hoje e identificador gerado
         /// automaticamente.
         /// </summary>
-        protected Pedido()
-        {
-            s_ultimoPedido++;
-            _idPedido = s_ultimoPedido;
-            _data = DateOnly.FromDateTime(DateTime.Now);
-            _itens = new LinkedList<IProduto>();
-            _aberto = true;
+        protected Pedido(DateOnly? data) {
+            init(data);
         }
 
         /// <summary>
@@ -73,8 +100,7 @@ namespace XulambsFoods_2025_1.src
         /// e quantidade de pizzas.
         /// </summary>
         /// <returns>TRUE/FALSE conforme seja permitido adicionar ou não</returns>
-        protected virtual bool PodeAdicionar()
-        {
+        protected virtual bool PodeAdicionar() {
             return _aberto;
         }
 
@@ -83,81 +109,114 @@ namespace XulambsFoods_2025_1.src
         /// comum a todos os pedidos.
         /// </summary>
         /// <returns>String multilinha com as informações acima</returns>
-        protected string DetalhamentoNota()
-        {
+        protected string DetalhesPedido() {
             StringBuilder relat = new StringBuilder($"nº{_idPedido} - {_data}\n");
             relat.AppendLine("==============================");
-            foreach (IProduto p in _itens) {
-                relat.AppendLine(p.ToString());
+            foreach (IProduto item in _itens) {
+                relat.AppendLine(item.ToString());
             }
+            relat.AppendLine($"\nValor dos itens: {ValorItens():C2}");
             return relat.ToString();
         }
 
         /// <summary>
-        /// Rodapé da nota de compra, com "bigode" e valor a pagar.
+        /// Rodapé da nota de compra, com o desconto e o valor a pagar.
         /// </summary>
-        /// <returns></returns>
-        protected string RodapeNota()
-        {
-            return $"-------------------------\nVALOR A PAGAR: {PrecoAPagar():C2}\n=========================";
+        /// <returns>String multilinha com as informações acima</returns>
+        protected string RodapeNotinha() {
+            StringBuilder relat = new StringBuilder();
+            relat.AppendLine($"Desconto do cliente: {_desconto:C2}");
+            relat.Append($"Valor a pagar: {PrecoAPagar():C2}");
+            return relat.ToString();
         }
 
         /// <summary>
-        /// Retorna o preço a pagar pelos itens do pedido, sem qualquer taxa incluída (valor double positivo).
+        /// Encapsula a lógica do cálculo do valor dos itens do pedidos (soma dos valores)
         /// </summary>
-        /// <returns>Double com o valor a pagar pelos itens do pedido.</returns>
-        protected double ValorItens()
-        {
-            double valor = 0d;
-            foreach (IProduto p in _itens)
-                valor += p.ValorFinal();
-            return valor;
-        }
-
-        /// <summary>
-        /// Tenta adicionar uma p ao pedido. Se não for possível, ignora a operação.
-        /// </summary>
-        /// <param name="item">A p a ser incluída no pedido</param>
-        /// <returns>Quantidade de pizzas no pedido após a execução.</returns>
-        public int Adicionar(IProduto item)
-        {
-            if (PodeAdicionar())
-            {
-                _itens.AddLast(item);
+        /// <returns>Valor dos itens do pedido (soma dos valores dos itens)</returns>
+        protected double ValorItens() {
+            double preco = 0d;
+            foreach (IProduto item in _itens) {
+                preco += item.ValorFinal();
             }
+            return preco;
+        }
+
+        /// <summary>
+        /// Tenta adicionar uma pizza ao pedido. Se não for possível, ignora a operação.
+        /// </summary>
+        /// <param name="item">A pizza a ser incluída no pedido</param>
+        /// <returns>Quantidade de pizzas no pedido após a execução.</returns>
+        /// <exception cref="InvalidOperationException">InvalidOperationException em caso de 
+        /// tentativa de alteração de pedido fechado</exception>
+        public int Adicionar(IProduto item) {
+            if (!PodeAdicionar()) {
+                throw new InvalidOperationException("Pedido fechado: não pode ser alterado.");
+            }
+            if (item == null)
+                throw new ArgumentNullException("Comida não foi criada corretamente");
+            _itens.AddLast(item);
             return _itens.Count;
         }
 
         /// <summary>
-        /// Fecha o pedido, caso ele tenha pelo menos 1 p. Caso contrário,
+        /// Fecha o pedido, caso ele tenha pelo menos 1 pizza. Caso contrário,
         /// ignora a operação.
         /// </summary>
-        public void FecharPedido(){
+        public void FecharPedido() {
             if (_itens.Count > 0)
-                throw new PedidoVazioException();
-
-            _aberto = false;
+                _aberto = false;
+            else
+                throw new InvalidOperationException("O pedido não pode ser fechado sem comidas");
         }
 
         /// <summary>
         /// Retorna o preço a pagar por um pedido (valor double positivo).
         /// </summary>
         /// <returns>Double com o valor a pagar pelo pedido.</returns>
-        public abstract double PrecoAPagar();
+        public virtual double PrecoAPagar() {
+            return ValorItens() - _desconto;
+        }
+
+        public double AplicarDesconto(double valor) {
+            if (valor < 0)
+                throw new ArgumentOutOfRangeException("Valor não pode ser negativo");
+            if (_desconto != 0)
+                throw new InvalidOperationException("Desconto já foi aplicado neste pedido");
+            _desconto = valor;
+            return _desconto;
+        }
+
+        public DateOnly Data() {
+            return _data;
+        }
+
+        public override bool Equals(object? obj) {
+            Pedido outroPedido = (Pedido)obj;
+
+            return this._idPedido == outroPedido._idPedido &&
+                    this._data.Equals(outroPedido._data);
+        }
+
         /// <summary>
         /// Retorna o id do pedido, para fins de comparação 
         /// (faremos melhor que isso em breve)
         /// </summary>
         /// <returns>Identificador do pedido (inteiro positivo)</returns>
-        public override int GetHashCode()
-        {
+        public override int GetHashCode() {
             return _idPedido;
         }
 
-        public override bool Equals(object? obj)
-        {
-            Pedido outroPedido = obj as Pedido;
-            return _idPedido == outroPedido._idPedido;
+        public int CompareTo(Pedido? other) {
+            int resposta = -1;
+            double precoDeste = this.PrecoAPagar();
+            double precoOutro = other.PrecoAPagar();
+            if (precoDeste == precoOutro)
+                resposta = 0;
+            else if (precoDeste > precoOutro)
+                resposta = 1;
+            return resposta;
+
         }
     }
 }
